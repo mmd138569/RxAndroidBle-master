0b850e99e9ea997a4b7d4d9633e152012ad2da7e
muhammad karim <muhammad138569@outlook.com>
2023-09-27 15:06:30 +0330

    try too find some way too turn the bluetoothon

--- sample/src/main/java/com/polidea/rxandroidble2/sample/example1_scanning/ScanActivity.java
+++ sample/src/main/java/com/polidea/rxandroidble2/sample/example1_scanning/ScanActivity.java
@@ -1,11 +1,19 @@
 package com.polidea.rxandroidble2.sample.example1_scanning;
 
+import android.Manifest;
 import android.animation.AnimatorInflater;
 import android.animation.AnimatorSet;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -49,6 +57,8 @@ public class ScanActivity extends AppCompatActivity {
     private ImageView imgloading;
     TextView scan1;
     TextView scan2;
+    BluetoothAdapter bluetoothAdapter;
+
     boolean a;
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -67,6 +77,29 @@ public class ScanActivity extends AppCompatActivity {
         animatorSet.start();
 
 //===================================================================
+
+      /*  if (ContextCompat.checkSelfPermission(ScanActivity.this, android.Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_DENIED) {
+            if (Build.VERSION.SDK_INT > 31) {
+                ActivityCompat.requestPermissions(ScanActivity.this, new String[]{Manifest.permission.BLUETOOTH_CONNECT}, 100);
+                return;
+            }
+        }
+        BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        if (Build.VERSION.SDK_INT >= 31) {
+            bluetoothAdapter = bluetoothManager.getAdapter();
+        } else {
+            bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        }
+        if (bluetoothAdapter.isEnabled()) {
+            bluetoothAdapter.disable();
+        } else {
+            bluetoothAdapter.isEnabled();
+        }*/
+
+//==========================================================
+
+
+
     }
 
    /* @OnClick(R.id.background_scan_btn)
