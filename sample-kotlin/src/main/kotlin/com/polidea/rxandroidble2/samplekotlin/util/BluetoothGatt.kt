19d766bf34d7a0b3c596a82e182028fbd6d06dee
muhammad karim <muhammad138569@outlook.com>
2023-09-27 09:00:28 +0330

    remove the kotlin project

--- sample-kotlin/src/main/kotlin/com/polidea/rxandroidble2/samplekotlin/util/BluetoothGatt.kt
+++
@@ -1,5 +0,0 @@
-package com.polidea.rxandroidble2.samplekotlin.util
-
-import android.bluetooth.BluetoothGattCharacteristic
-
-fun BluetoothGattCharacteristic.hasProperty(property: Int): Boolean = (properties and property) > 0
--- sample-kotlin/src/main/kotlin/com/polidea/rxandroidble2/samplekotlin/util/ByteArray.kt
+++
@@ -1,3 +0,0 @@
-package com.polidea.rxandroidble2.samplekotlin.util
-
-fun ByteArray.toHex() = joinToString("") { String.format("%02X", (it.toInt() and 0xff)) }
