7113844e520305da893e048d3eac910df8406007
muhammad karim <65225453+mmd138569@users.noreply.github.com>
2023-10-24 11:53:02 +0330

    Add the main acc

---
+++ sample/src/main/java/com/polidea/rxandroidble2/sample/MainActivity.java
@@ -0,0 +1,46 @@
+package com.polidea.rxandroidble2.sample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    ImageView img;
+    Animation bottom_animation,top_animation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        bottom_animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.animation);
+        img = findViewById(R.id.imglogo);
+        img.setAnimation(bottom_animation);
+        top_animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.textanimation);
+        textView = findViewById(R.id.txt);
+        textView.setAnimation(top_animation);
+        Handler h = new Handler();
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                Intent in = new Intent(MainActivity.this, warning.class);
+                startActivity(in);
+                finish();
+            }
+        };
+        h.postDelayed(r, 1500);
+    }
+}
