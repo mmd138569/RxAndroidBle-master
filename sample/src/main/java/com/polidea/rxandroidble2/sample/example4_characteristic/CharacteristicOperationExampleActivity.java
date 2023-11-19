c34d1db1c886d02c09e922dc6653967f5df75b6a
muhammad karim <65225453+mmd138569@users.noreply.github.com>
2023-11-19 15:40:04 +0330

    Update CharacteristicOperationExampleActivity.java

--- sample/src/main/java/com/polidea/rxandroidble2/sample/example4_characteristic/CharacteristicOperationExampleActivity.java
+++ sample/src/main/java/com/polidea/rxandroidble2/sample/example4_characteristic/CharacteristicOperationExampleActivity.java
@@ -135,7 +135,17 @@ public class CharacteristicOperationExampleActivity extends AppCompatActivity {
         Handler handler=new Handler();
         Handler hand=new Handler();
         Handler nand =new Handler();
-      /*  handler.postDelayed(new Runnable() {
+        if (!foregroundServiceRunning()) {
+           /* Intent serviceinetnt = new Intent(CharacteristicOperationExampleActivity.this, myservice.class);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                startForegroundService(serviceinetnt);
+            }*/
+            Intent in= new Intent(CharacteristicOperationExampleActivity.this, myservice.class);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                startForegroundService(in);
+            }
+        }
+        handler.postDelayed(new Runnable() {
             @Override
             public void run() {
 
@@ -164,7 +174,7 @@ public class CharacteristicOperationExampleActivity extends AppCompatActivity {
                 hand.postDelayed(this, 17000);
 
             }
-        },17000);*/
+        },17000);
 
        // thread();
         lineChart.setOnClickListener(new View.OnClickListener() {
@@ -175,16 +185,7 @@ public class CharacteristicOperationExampleActivity extends AppCompatActivity {
                 finish();
             }
         });
-        if (!foregroundServiceRunning()) {
-           /* Intent serviceinetnt = new Intent(CharacteristicOperationExampleActivity.this, myservice.class);
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                startForegroundService(serviceinetnt);
-            }*/
-            Intent in= new Intent(CharacteristicOperationExampleActivity.this, myservice.class);
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                startForegroundService(in);
-            }
-        }
+
 //==================================================
     }
     /*public static void thread(){
