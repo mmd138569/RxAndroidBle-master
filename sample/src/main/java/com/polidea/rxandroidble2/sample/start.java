6ae6abd529e7b9d0943243a45b56e8faf5146eeb
muhammad karim <65225453+mmd138569@users.noreply.github.com>
2023-11-26 15:47:27 +0330

    location for some android version

--- sample/src/main/java/com/polidea/rxandroidble2/sample/start.java
+++ sample/src/main/java/com/polidea/rxandroidble2/sample/start.java
@@ -1,9 +1,13 @@
 package com.polidea.rxandroidble2.sample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.ActivityOptions;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -28,10 +32,23 @@ public class start extends AppCompatActivity {
         starting.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(start.this,
+                        Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(start.this,
+                            Manifest.permission.ACCESS_FINE_LOCATION)){
+                        ActivityCompat.requestPermissions(start.this,
+                                new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                    }else{
+                        ActivityCompat.requestPermissions(start.this,
+                                new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                    }
+                }
+                
                 Intent in = new Intent(start.this, mainlogin.class);
                 ActivityOptions options =
                         ActivityOptions.makeCustomAnimation(start.this, R.anim.animationint, R.anim.anim);
                 start.this.startActivity(in, options.toBundle());
+
 
             }
         });
