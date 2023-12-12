6049242f26ea6832ce4a51ec353609517787ee5b
muhammad karim <65225453+mmd138569@users.noreply.github.com>
2023-12-12 08:42:46 +0330

    Update MainActivity.java

--- sample/src/main/java/com/polidea/rxandroidble2/sample/MainActivity.java
+++ sample/src/main/java/com/polidea/rxandroidble2/sample/MainActivity.java
@@ -20,14 +20,17 @@ import android.widget.TextView;
 import com.polidea.rxandroidble2.sample.example1_scanning.ScanActivity;
 
 public class MainActivity extends AppCompatActivity {
-    SharedPreferences preferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
-    boolean isFirstTime = preferences.getBoolean("isFirstTime", true);
+
     TextView textView;
     ImageView img;
     Animation bottom_animation,top_animation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        SharedPreferences preferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        boolean isFirstTime = preferences.getBoolean("isFirstTime", true);
+
         super.onCreate(savedInstanceState);
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
@@ -48,19 +51,28 @@ public class MainActivity extends AppCompatActivity {
                     if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
                         //Intent in = new Intent(MainActivity.this, EntertransmitterSN1.class);
                         //Intent in = new Intent(MainActivity.this, EntertransmitterSN1.class);
+                        SharedPreferences.Editor editor = preferences.edit();
+                        editor.putBoolean("isFirstTime", false);
+                        editor.apply();
+
                         Intent in = new Intent(MainActivity.this, warning.class);
                         startActivity(in);
                         finish();
                     }
-                        else if(ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.BLUETOOTH_CONNECT)== PackageManager.PERMISSION_DENIED){
+                    else if(!isFirstTime){
+                        Intent in = new Intent(MainActivity.this, mainlogin.class);
+                        startActivity(in);
+                        finish();
+                    }
+                    else if(ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.BLUETOOTH_CONNECT)== PackageManager.PERMISSION_DENIED){
                             Intent in = new Intent(MainActivity.this, bluetooth.class);
                             startActivity(in);
                             finish();
-                        }
-                        else {
-                            Intent in = new Intent(MainActivity.this, EntertransmitterSN1.class);
-                            startActivity(in);
-                            finish();
+                    }
+                    else {
+                        Intent in = new Intent(MainActivity.this, EntertransmitterSN1.class);
+                        startActivity(in);
+                        finish();
                         }
                 }
                 else if(isFirstTime){
@@ -73,7 +85,7 @@ public class MainActivity extends AppCompatActivity {
                     finish();
 
                 }
-                else if(!isFirstTime){
+                else{
                     Intent in = new Intent(MainActivity.this, mainlogin.class);
                     startActivity(in);
                     finish();
