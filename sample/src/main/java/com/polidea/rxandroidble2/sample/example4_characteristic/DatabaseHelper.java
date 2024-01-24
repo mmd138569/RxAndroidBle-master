01ba4bef1fb3931b9acbf3dc8d22a3f1ee410086
muhammad karim <65225453+mmd138569@users.noreply.github.com>
2024-01-24 11:11:27 +0330

    Update DatabaseHelper.java

--- sample/src/main/java/com/polidea/rxandroidble2/sample/example4_characteristic/DatabaseHelper.java
+++ sample/src/main/java/com/polidea/rxandroidble2/sample/example4_characteristic/DatabaseHelper.java
@@ -87,23 +87,6 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         }
         return array_list1;
     }
-    /*public ArrayList getdata(){
-        SQLiteDatabase db = this.getReadableDatabase();
-        ArrayList<String> array_list = new ArrayList<String>();
-
-        Calendar calendar = Calendar.getInstance(); // This will give you the current time.
-        // Removing the timestamp from current time to point to todays date
-        calendar.set(Calendar.HOUR_OF_DAY, 0);
-        calendar.set(Calendar.MINUTE, 0);
-        calendar.set(Calendar.SECOND, 0);
-        calendar.set(Calendar.MILLISECOND, 0);
-        calendar.add(Calendar.DATE, -1); // Will subtract 3 days from today.
-        Date adaybefore = calendar.getTime();
-        calendar.add(Calendar.DATE, 0); // Will be your 3 days after today
-        Date today = calendar.getTime();
-
-        db.query("fullname", null, "YOUR_DATE_COLUMN >= ? AND YOUR_DATE_COLUMN <= ?", new String[] { adaybefore.getTime() + "", today.getTime() + "" }, null, null, null);
-    }*/
     public boolean update(String s, String s1) {
         SQLiteDatabase db = this.getWritableDatabase();
         db.execSQL("UPDATE "+CONTACTS_TABLE_NAME+" SET name = "+"'"+s+"', "+ "salary = "+"'"+s1+"'");
