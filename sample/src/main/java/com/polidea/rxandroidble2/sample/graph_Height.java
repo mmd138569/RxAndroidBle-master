1f6a7829ad62c89c95c3b0e1cdc2a3c5e00b8669
mmd <muhammad138569@gmail.com>
2024-02-10 12:33:42 +0330

    graph height

--- sample/src/main/java/com/polidea/rxandroidble2/sample/graph_Height.java
+++ sample/src/main/java/com/polidea/rxandroidble2/sample/graph_Height.java
@@ -3,13 +3,18 @@ package com.polidea.rxandroidble2.sample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class graph_Height extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_graph_height);
-        
+
     }
 }
