10c6dee28269953d02d72633a2e5d88a2a2d0edd
mmd <muhammad138569@gmail.com>
2024-02-10 15:08:41 +0330

    try to save the changes on the DBchart

---
+++ sample/src/main/java/com/polidea/rxandroidble2/sample/DBChart.java
@@ -0,0 +1,98 @@
+package com.polidea.rxandroidble2.sample;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class DBChart extends SQLiteOpenHelper {
+    public static final String DBChar = "chart.db";
+    public static final String HeightChart_Table_name = "heightchart";
+
+    public DBChart(Context context) {
+        super(context, DBChar, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        try {
+            db.execSQL("CREATE TABLE " + HeightChart_Table_name + "(id INTEGER PRIMARY KEY,heightChart DECIMAL(4,2))");
+        } catch (SQLiteException e) {
+            try {
+                throw new IOException(e);
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + HeightChart_Table_name);
+        onCreate(db);
+    }
+
+
+    public boolean insert(int s1) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        //contentValues.put("id", s);
+        contentValues.put("heightChart", s1);
+        //db.replace(CONTACTS_TABLE_NAME, null, contentValues);
+        db.insert(HeightChart_Table_name, null, contentValues);
+        return true;
+    }
+
+
+    public ArrayList getAllCotact1() {
+        SQLiteDatabase db1 = this.getReadableDatabase();
+        ArrayList<String> array_list1 = new ArrayList<String>();
+        Cursor res = db1.rawQuery("select ( heightChart ) AS char from " + HeightChart_Table_name, null);
+        res.moveToFirst();
+        while (res.isAfterLast() == false) {
+//========================================================
+
+            if (res.getColumnIndex("char") != -1) {
+                array_list1.add(res.getString(res.getColumnIndex("char")));
+            }
+
+//=========================================================
+            res.moveToNext();
+        }
+        return array_list1;
+    }
+
+
+    /*public ArrayList getdata(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        ArrayList<String> array_list = new ArrayList<String>();
+
+        Calendar calendar = Calendar.getInstance(); // This will give you the current time.
+        // Removing the timestamp from current time to point to todays date
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        calendar.add(Calendar.DATE, -1); // Will subtract 3 days from today.
+        Date adaybefore = calendar.getTime();
+        calendar.add(Calendar.DATE, 0); // Will be your 3 days after today
+        Date today = calendar.getTime();
+
+        db.query("fullname", null, "YOUR_DATE_COLUMN >= ? AND YOUR_DATE_COLUMN <= ?", new String[] { adaybefore.getTime() + "", today.getTime() + "" }, null, null, null);
+    }*/
+    public boolean update(String s, String s1) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL("UPDATE "+HeightChart_Table_name+" SET name = "+"'"+s+"', "+ "salary = "+"'"+s1+"'");
+        return true;
+    }
+    public boolean delete() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL("DELETE from "+HeightChart_Table_name);
+        return true;
+    }
+}
--- sample/src/main/java/com/polidea/rxandroidble2/sample/graph_Height.java
+++ sample/src/main/java/com/polidea/rxandroidble2/sample/graph_Height.java
@@ -3,8 +3,12 @@ package com.polidea.rxandroidble2.sample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.RadioButton;
+
+import java.util.ArrayList;
 
 public class graph_Height extends AppCompatActivity {
 
@@ -15,6 +19,30 @@ public class graph_Height extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_graph_height);
+        RadioButton radioButton=findViewById(R.id.min_hight);
+        RadioButton radioButton1= findViewById(R.id.max_hight);
 
+        final DBChart helper = new DBChart(graph_Height.this);
+        final ArrayList array_list = helper.getAllCotact1();
+        radioButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int x = Integer.parseInt("300");
+                if (x != 0) {
+                    helper.insert(x);
+                    System.out.println(helper.getAllCotact1() + "=============================");
+                }
+            }
+        });
+        radioButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int x = Integer.parseInt("400");
+                if (x != 0) {
+                    helper.insert(x);
+                    System.out.println(helper.getAllCotact1() + "=============================");
+                }
+            }
+        });
     }
 }
