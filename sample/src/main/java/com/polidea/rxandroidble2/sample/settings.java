fe181324361c705ead1dac16ecab24b09d29c48c
mmd <muhammad138569@gmail.com>
2024-04-07 16:32:56 +0330

    remove some package

--- sample/src/main/java/com/polidea/rxandroidble2/sample/settings.java
+++ sample/src/main/java/com/polidea/rxandroidble2/sample/settings.java
@@ -1,7 +1,6 @@
 package com.polidea.rxandroidble2.sample;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.room.Room;
 
 import android.content.Intent;
 import android.os.Bundle;
