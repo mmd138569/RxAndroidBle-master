7fd4c43a2f67c9e227f71d36fff9511cb4be9dce
mmd <muhammad138569@gmail.com>
2024-04-09 11:59:28 +0330

    Update CharacteristicOperationExampleActivity.java

--- sample/src/main/java/com/polidea/rxandroidble2/sample/example4_characteristic/CharacteristicOperationExampleActivity.java
+++ sample/src/main/java/com/polidea/rxandroidble2/sample/example4_characteristic/CharacteristicOperationExampleActivity.java
@@ -290,9 +290,9 @@ public class CharacteristicOperationExampleActivity extends AppCompatActivity {
                 if(!isConnected()) {
                     onConnectToggleClick();
                 }
-                nand1.postDelayed(this, 500);
+                nand1.postDelayed(this, 1000);
             }
-        },500);
+        },1000);
 
        /* nand2.postDelayed(new Runnable() {
             @Override
